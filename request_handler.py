124e54707368af3aea6167b398de1051d2ad26d1
Michael Baumer <meeesterbaumer@gmail.com>
2020-10-07 16:16:06 -0500

    DOne with all

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal
 from locations import get_all_locations, get_single_location
+from customers import get_all_customers, get_single_customer
+from employees import get_all_employees, get_single_employee
 
 
 # Here's a class. It inherits from another class.
@@ -35,6 +37,20 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             else:
                 response = f"{get_all_locations()}"
+
+        if resource == "customers":
+            if id is not None:
+                response = f"{get_single_customer(id)}"
+
+            else:
+                response = f"{get_all_customers()}"
+
+        if resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
+
+            else:
+                response = f"{get_all_employees()}"
 
         self.wfile.write(response.encode())
 
